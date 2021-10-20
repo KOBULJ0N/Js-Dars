9933586fa20ff91673d372739d0e419e47c5f057
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2021-10-20 22:04:49 +0500

    just some math exercise

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-console.log('Hello Js')
+////////////   BEGIN 1 ///////////////
+
+// var a = 4
+// //   p = 4 * a //
+// console.log(4 * a);
+
+
+/////////   BEGIN 2 //////////////////
+
+// var a = 10;
+// console.log(a * a);
+
+/////////   BEGIN 3 //////////////////
+
+// var a = 6
+// var b = 9
+// console.log(a * b); 
+// console.log(2 * (a + b));
+
+/////////   BEGIN 4 //////////////////
+
+// var d = 18;
+// var p = 3.14;
+// console.log(p * d);
+
+/////////   BEGIN 5 //////////////////
+
+//  var a = 10;
+// console.log(a * a * a);
+// console.log(6 * (a * a));
+
+/////////   BEGIN 6 //////////////////
+
+// var a = 6;
+// var b = 9;
+// var c = 13;
+
+// console.log(a * b * c);
+// console.log( 2*(a * b + b * c +a * c));
+
+/////////   BEGIN 7 //////////////////
+
+// var R = 25;
+// var P = 3.14;
+// console.log(2 * P * R);
+// console.log(P * R * R);
+
+
+/////////   BEGIN 7 //////////////////
+
+// var a = 26;
+// var b = 68;
+// console.log((a + b) / 2);
+
+/////////   BEGIN 8 //////////////////
+
+// var a = 81;
+// var b = 49;
+// console.log(a / a * b / b);
+
+
+/////////   BEGIN 12 //////////////////
+
+var a = 12;
+var b = 6;
+var c = a * a + b * b
+
