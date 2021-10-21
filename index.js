05e7804613321c26641e4847c460d09025aea0eb
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2021-10-21 16:08:41 +0500

    new tasks

--- index.js
+++ index.js
@@ -1,67 +1,172 @@
 ////////////   BEGIN 1 ///////////////
 
 // var a = 4
-// //   p = 4 * a //
-// console.log(4 * a);
-
+// var p = 4 * a
+// console.log(p);
 
 /////////   BEGIN 2 //////////////////
 
 // var a = 10;
-// console.log(a * a);
+// var S= a ** 2
+// console.log(S);
 
 /////////   BEGIN 3 //////////////////
 
 // var a = 6
 // var b = 9
-// console.log(a * b); 
-// console.log(2 * (a + b));
+// var S = a * b
+// var P = 2 * (a + b)
+// console.log(S);
+// console.log(P);
 
 /////////   BEGIN 4 //////////////////
 
 // var d = 18;
 // var p = 3.14;
-// console.log(p * d);
+// var L = p * d
+// console.log(L);
 
 /////////   BEGIN 5 //////////////////
 
 //  var a = 10;
-// console.log(a * a * a);
-// console.log(6 * (a * a));
+//  var V = a **3
+//  var S = 6 * (a ** 2)
+//  console.log(V);
+//  console.log(S);
 
 /////////   BEGIN 6 //////////////////
 
 // var a = 6;
 // var b = 9;
 // var c = 13;
-
-// console.log(a * b * c);
-// console.log( 2*(a * b + b * c +a * c));
+// var V = a * b * c
+// var S = 2*(a * b + b * c +a * c)
+// console.log(V);
+// console.log(S);
 
 /////////   BEGIN 7 //////////////////
 
 // var R = 25;
 // var P = 3.14;
-// console.log(2 * P * R);
-// console.log(P * R * R);
+// var L = 2 * P * R
+// var S = P * R ** 2
+// console.log(L);
+// console.log(S);
 
-
-/////////   BEGIN 7 //////////////////
+/////////   BEGIN 8 //////////////////
 
 // var a = 26;
 // var b = 68;
 // console.log((a + b) / 2);
 
-/////////   BEGIN 8 //////////////////
+/////////   BEGIN 9 //////////////////
 
-// var a = 81;
-// var b = 49;
-// console.log(a / a * b / b);
+/////////////////////////////////////
 
+/////////   BEGIN 10 //////////////////
+// a is not equal to 0
+
+// var a = 10;
+// var b = 20;
+// var Y = a + b;
+// var S = a * b;
+// var K1 = a ** 2  ;
+// var K2 = b ** 2  ;
+// console.log(Y);
+// console.log(K1, K2);
+// console.log(S);
+
+/////////   BEGIN 11 //////////////////
+// a is not equal to 0
+
+// var a = 10;
+// var b = 20;
+// var Y = a + b;
+// var S = a * b;
+// var M = ?
+// console.log(Y);
+// console.log(S);
+
+/////////////////////////////////////
 
 /////////   BEGIN 12 //////////////////
 
-var a = 12;
-var b = 6;
-var c = a * a + b * b
+//////////////////////////////////////
 
+/////////   BEGIN 13 //////////////////
+//    R1 > R2
+// var R1 = 12;
+// var R2 = 10;
+// var P = 3.14;
+// var S1 = P * R1;
+// var S2 = P * R2;
+// var S3 = P * (R1 - R2);
+// console.log(S1, S2, S3);
+
+//////////////////////////////////////
+
+
+
+/////////   BEGIN 14 //////////////////
+// var P = 3.14;
+// var D = 40;
+// var R = D / 2;
+// var L = 2 * P * R;
+// var S = P * R ** 2;
+// console.log(S, R);
+
+/////////   BEGIN 19 //////////////////
+
+// var a = 10;
+// var b = 8;
+// var P = 2 * (a + b);
+// var S = a * b;
+// console.log(P, S);
+
+
+/////////   BEGIN 17 //////////////////
+
+// var A = 10;
+// var B = 16;
+// var C = 25;
+// var AB = B - A;
+// var BC = C - B;
+// var U = AB + BC
+// console.log(U);
+
+/////////   BEGIN 18 //////////////////
+
+// var A = 10;
+// var B = 16;
+// var C = 25;
+// var AB = B - A;
+// var BC = C - B;
+// var U = AB * BC
+// console.log(U);
+
+/////////   BEGIN 22 //////////////////
+
+// var a = 10;
+// var b = 20;
+// var c = a;
+
+// a = b;
+// b = c;
+
+// console.log(a, b)
+
+
+/////////   BEGIN 23 //////////////////
+
+// var a = 10;
+// var b = 20;
+// var c = 30;
+// var d = a;
+
+// a = b;
+// b = c;
+// c = d;
+// console.log(a, b, c)
+
+
+//////////////////////////////////////
