657c29b7156adb8384010a8020417b1b0a49f9ee
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2021-11-04 15:47:35 +0500

    task file created

--- script2.js
+++ script2.js
@@ -1,15 +1,45 @@
 // Write a function that inserts a white space between every instance of a lower character followed immediately by an upper character.
-// Examples
-
-// insertWhitespace("SheWalksToTheBeach") ➞ "She Walks To The Beach"
-
-// insertWhitespace("MarvinTalksTooMuch") ➞ "Marvin Talks Too Much"
-
-// insertWhitespace("TheGreatestUpsetInHistory") ➞ "The Greatest Upset In History"
-
+// Examples :
 /*     task №4    */
-let stringCamelCase = 'myNameIsQobiljon';
+let CamelCase = 'myNameIsQobiljon';
 
-let string = stringCamelCase.split(/(?=[A-Z])/).join(' ');
+let string = CamelCase.split(/(?=[A-Z])/).join(' ');
 
 console.log(string);
+
+
+
+// Write a function that takes an integer i and returns an integer with the integer backwards followed by the original integer.
+// To illustrate:
+// 123
+// We reverse 123 to get 321 and then add 123 to the end, resulting in 321123.
+
+// reverseAndNot(123) ➞ 321123
+
+// reverseAndNot(152) ➞ 251152
+
+// reverseAndNot(123456789) ➞ 987654321123456789
+
+// let num = 123;
+// for (i = num.length, i < 0, i--) {
+//     console.log()
+// }
+
+
+// Create a function that takes in an array (slot machine outcome) and returns true if all elements in the array are identical, and false otherwise. The array will contain 4 elements.
+// Examples
+// testJackpot(["@", "@", "@", "@"]) ➞ true
+
+// let obj = ['q', 'q', 'q', 'q', 'q','q'];
+// let c = 0;
+
+// for (i = 0; i <= obj.length; i++) {
+//     if (obj[i] == obj[i + 1])
+//         c++;
+//     }
+// if (c == obj.length) {
+//   console.log('true');
+// } else {
+//   console.log('false');
+// }
+
