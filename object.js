276ad5cc7ea9282b92c15de3fdc49b647644a15e
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2021-11-10 11:54:43 +0500

    mission accomplished

--- object.js
+++ object.js
@@ -62,23 +62,20 @@
 
 // console.log(myObject.cars[0].models[2]);
 
-
-
-
-
-
-
-
-
-
 /* 1. Object kalitlari va qiymatlarini tengligini tekshirish */
 
 // const data = { a: 1, b: 1 };
 // const data2 = { a: 1, b: 1 };
 // const data3 = { a: 1, b: 2 };
 
-// areEqual( data, data2 )  => true
-// areEqual( data, data3 )  => false
+// const areEqual = (value1, value2) => {
+//   if (Object.values(value1).toString() === Object.values(value2).toString()) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// };
+// areEqual(data, data2);
 
 // 2. Object ichidagi Objectni qiymatlari mavjudligini tekshirish
 
@@ -86,32 +83,35 @@
 // const data2 = { a: { b: 'it does exits' } };
 // const data3 = { a: { b: 'it does exits', innerKey: null } };
 
-// nestedValueExists(data)  => false
-// nestedValueExists(data2)  => true
-// nestedValueExists(data3)  => false
+// const nestedValueExists = (obj) => {
+//   if (obj.a.b === undefined) {
+//     return false
+//   }
+//   else if (obj.a.innerKey === null) {
+//     return false
+//   }else return true
+// }
+
+// console.log(nestedValueExists(data));
+// console.log(nestedValueExists(data2));
+// console.log(nestedValueExists(data3));
 
 // 3. Object qiymatlari va kalitlari bir xillari borligini tekshirish
 
-// const data = { a: 1, b: 2 };
-// const data2 = { c: 1, b: 2 };
-// const data3 = { c: 3, b: 5 };
+const data = { b: 2,  a: 1, };
+const data2 = {b: 2,  c: 1,  };
+const data3 = { c: 3, b: 5 };
 
-// checkSame( data, data2 )  => true
-// checkSame( data2, data3 )  => false
-
-
-
-
-const data = { a: 1, b: 1 };
-const data2 = { a: 1, b: 1 };
-const data3 = { a: 1, b: 2 };
-
-const areEqual = (value1, value2) => {
-  if (Object.values(value1).toString() === Object.values(value2).toString()) {
-    console.log(true);
+const checkSame = (value1, value2) => {
+  if(
+    Object.values(value1).toString() === Object.values(value2).toString() ||
+    Object.keys(value1).toString() === Object.keys(value2).toString()
+  ) {
+    return true;
   } else {
-    console.log(false);
+    return false;
   }
-}
+};
 
-areEqual(data, data2);
+console.log(checkSame(data, data2));
+console.log(checkSame(data2, data3));
