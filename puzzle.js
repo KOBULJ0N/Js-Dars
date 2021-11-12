9cc80fd7f1599e52f4feeb769c6f13fffefbc3ec
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2021-11-12 23:30:16 +0500

    puzzle solved

---
+++ puzzle.js
@@ -0,0 +1,77 @@
+// const univ = [
+//     {
+//         id: 1,
+//         name: 'Eshmat',
+//         status: 'CS',
+//         department: 'IT',
+//     },
+//     {
+//         id: 2,
+//         name: 'Toshmat',
+//         status: 'BBA',
+//         department: 'IT',
+//     },
+//     {
+//         id: 3,
+//         name: 'Eldor',
+//         status: 'CS',
+//         department: 'IT',
+//     },
+//     {
+//         id: 0,
+//         name: 'Gulbashakar',
+//         status: 'CS',
+//         department: 'AI',
+//     },
+//     {
+//         id: 10,
+//         name: 'Mirshakar',
+//         status: 'CS',
+//         department: 'AI',
+//     },
+// ];
+
+// console.log(Number(1010).toString(10));
+// console.log (parseInt(1001, 2))
+// console.log(-(5 - -'6' ));
+// console.log((parseFloat(10 / 4 ).toFixed(1)))
+// console.log(parseFloat("years 10"));
+
+// console.log(Math.min(21.89, Math.round(20.8), Math.max(Math.ceil(23.1), 23),Math.min(Math.floor(26.89), 22)));
+
+// var a = '8' + '7';
+// console.log (a)
+
+// console.log(Math.max(Math.max(21.89, Math.round(19.99), Math.max(Math.ceil(23.1, 1)), parseFloat('5'), Math.min(Math.floor(23.89), 21.890))));
+
+//  var ar = 'salom';  console.log(ar.padStart(8));
+// var ar = 'salom'; console.log(ar.replace(ar[0], ar[ar.length -2]))
+
+// var ar = 'welcome!'
+// console.log(ar.slice(2));
+
+// var a = [
+//   ['table tennis', 'tennis'],
+//   ['football', 'soccer'],
+//   ['basketball', 'valleyball'],
+// ];
+// console.log(a[2].pop());
+
+// var b = ['table tennis', 'tennis', 'football', 'soccer'];
+// console.log(b.shift());
+// var ar = [12, 3, 4, 5, 6, 7, 4, 3, 5];
+
+// while (ar.length > 1) {
+//     ar.sort((a, b) => b - a);
+//     if (ar[0] - ar[1] === 0) {
+//         console.log(ar[0] - ar[1]);
+//         ar.splice(o, 2);
+//     } else {
+//         console.log(ar[0] - ar[1]);
+//         ar.splice(0, 1);
+//         ar.shift();
+//         ar[]
+//     }
+// }
+
+console.log(Number(true),Number(false),Number(false));
