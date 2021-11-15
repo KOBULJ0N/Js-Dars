7b9872c09a1c24b66c071ffe4363c5f3b8fefeef
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2021-11-15 18:52:03 +0500

    js question test

---
+++ test.js
@@ -0,0 +1,74 @@
+// console.log(Math.max(Math.round(20.8),Math.min(Math.ceil(23.1),(Math.floor(26.89)))));
+// console.log((parseFloat(10 / 4 ).toFixed(1)));
+// var ar = 'salom'; console.log(ar.replace(ar[0], ar[ar.length -2]))
+// var a = true; var b = '331s'; console.log(typeof (b * Number(a)));
+
+// var a = { name: 'qobuljon' }
+// var b = ['ism qobiljon']
+// console.log(` ${typeof(a)}  + ' ' + ${typeof(b)}`);
+
+/* 41. What is break and continue statements?
+
+Break statement exits from the current loop.
+
+Continue statement continues with next statement of the loop.  */
+
+/* what is the difference between these two arrays 
+var ar1 = [ something, something1];
+var ar1 = new ar(something, something2)
+
+
+Object ga yangi qiymat qoshishni 2 hil yolini korsating
+
+
+For loop da 3 chi qiymat berilmasa nima sodir boladi?
+
+*/
+
+// foo = (a, b, c) => {console.log(a + b + c);}
+// foo('hello ','there ');
+// const func = function (a, b) {
+//     return arguments.length
+// }
+
+// const result = func(null, undefined, true, false)
+// console.log(result);
+
+// const person = {
+//     age: 20,
+//     name: 'Ben',
+//     status: 'student'
+// };
+// const Arr = [person, person, person];
+// console.log( Arr[1].name);
+// // console.log(Arr[2].name);
+
+// const N1_UfcFighters = []
+// N1_UfcFighters[1] = 'Khabib'; N1_UfcFighters[2] = 'Jon Jones';N1_UfcFighters[2] = 'Kamaru Usman';console.log(N1_UfcFighters);
+
+// const fruits = ['Banana', 'Orange', 'Apple'];
+// fruits[6] = 'Lemon';
+
+// reduce nima vazifani bajaradi ?
+
+// const length = 4;
+// const numbers = [];
+// for (var i = 0; i < length; i++);{
+//   numbers.push(i + 1);
+// }
+
+// console.log(numbers);
+
+// var person = {
+//     name: 'john',
+//     age: 22,
+//     job:'Frontend Developer'
+// }
+// console.log(person.age);
+// person.age = 23
+// a = -12;
+// if (a >= 0) {
+//   var result = 'positive';
+// } else if (a === 0) { var result = 'zero'; } else { var result = 'negative'; }
+
+// console.log(result);
